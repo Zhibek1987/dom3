0224a1c62ec59ffd24ef67c7b12599823b24be59
Zhibek1987 <zhikabeka@gmail.com>
2023-12-23 17:04:26 +0600

    randomcoloer

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ for (let i=1; i< n;i++) {
     a.setAttribute('class', 'a');
     container.append(a)
     a.addEventListener('mouseover', ()=>{
-        a.style.background='greenyellow'
+        a.style.background= randomColor()
     })
     a.addEventListener('mouseleave', ()=>{
         a.style.background=''
     })
+}
+const randomColor=()=> {
+    let b = '#'
+    for (let i=1; i<=6;i++){
+        b += Math.floor(Math.random()* 10)
+
+    }
+    return b
 }
